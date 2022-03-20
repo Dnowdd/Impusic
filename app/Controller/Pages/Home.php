84d26be7a7c0d19dafdae44946e072b7a5b49e3c
David Aquino <dqueiroz2005@gmail.com>
2022-03-20 16:36:58 -0300

    Testes

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -4,6 +4,7 @@ namespace App\Controller\Pages;
 
 use \App\Utils\View;
 use \App\Model\Entity\Organization;
+use \App\Model\Entity\Videos as EntityVideos;
 
 class Home extends Page{
     public static function getPiece($name){
@@ -15,12 +16,54 @@ class Home extends Page{
         return $itens;
     }
 
+    public static function time_elapsed_string($datetime, $full = false) {
+        $now = new \DateTime;
+        $ago = new \DateTime($datetime);
+        $diff = $now->diff($ago);
+    
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+    
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+    
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . '' : 'just now';
+    }
+
     public static function getVideoBox(){
         $itens = '';
+        $videoCard = '';
+
+        $results = EntityVideos::getVideos(null,'id DESC');
+
+        while($obUser = $results->fetchObject(EntityVideos::class)){
+            $time = self::time_elapsed_string('@'.$obUser->date);
+
+            $videoCard .= View::render('pages/home/videoCard',[
+                'videoTitle' => $obUser->title,
+                'channel' => $obUser->channel,
+                'thumbnail' => str_replace(array("\r", "\n"), '',$obUser->thumbnail),
+                'time' => $time
+            ]);
+        } 
 
         $itens .= View::render('pages/home/videoBox', [
-            'videoCard' => View::render('pages/home/videoCard', [
-            ])
+            'videoCard' => $videoCard
         ]);
 
         return $itens;
@@ -38,7 +81,7 @@ class Home extends Page{
         $content = View::render('pages/home',[
             'hero' => self::getPiece('hero'),
             'banner' => self::getPiece('banner'),
-            'videoCard' => self::getVideoBox()
+            'videoBox' => self::getVideoBox()
         ]);
 
         //RETORNA A VIEW DA PÁGINA
--- resources/js/includes/header.js
+++ resources/js/includes/header.js
@@ -1,7 +1,6 @@
 function locationVerification() {
     var e = document.body.scrollTop || document.documentElement.scrollTop,
     o = e / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
-    console.log(o);
     if(e > 0){
         $('#header').addClass("sticky");
     }
